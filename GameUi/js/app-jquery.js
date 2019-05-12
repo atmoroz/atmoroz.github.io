d94eb6cd63a80ec3e785a15e678fa1d35bdf3f02
atmoroz <atmoroz93@gmail.com>
2019-05-12 20:00:25 +0300

    replace all

--- GameUi/js/app-jquery.js
+++ GameUi/js/app-jquery.js
@@ -1,16 +1,12 @@
-const INFORMATIONICONS = $('.information_icons');
 $('.menu')
     .on('click', '.menu_item', function() {
         $(this)
         .addClass("active")
         .siblings().removeClass("active");
 
-        INFORMATIONICONS.removeClass("active");
-        $(this).find(INFORMATIONICONS).addClass("active");
-
-        let tabs = ($(this).attr("id")).replace('#','.');
+        let tabs = ($(this).attr("id"));
         //активируем нужный нам таб (привязываемся к id) после удаляем соседей
-        $(tabs)
+        $('.'+ tabs)
         .addClass("active_display")
         .siblings()
         .removeClass("active_display");
